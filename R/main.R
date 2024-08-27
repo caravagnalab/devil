628591ed131de097bdb6744f362df33fcce308c0
Giovanni Santacatterina <65920542+jovoni@users.noreply.github.com>
2024-08-27 16:39:18 +0200

    Update documentation and vignettes

--- R/main.R
+++ R/main.R
@@ -1,30 +1,52 @@
-
 #' Fit Model Parameters
 #'
-#' This function fits model parameters including beta coefficients, dispersion parameter,
-#' and beta sigma using provided predictor variables (X) and response variable (y).
+#' This function fits model parameters, including beta coefficients, the dispersion parameter,
+#' and beta sigma, using the provided predictor variables (`design_matrix`) and response variable (`input_matrix`).
 #' It optionally estimates overdispersion based on the fitted model.
 #'
 #' @description
-#' This function fits model parameters including beta coefficients, dispersion parameter,
-#' and beta sigma using provided predictor variables (input_matrix) and response variable (design_matrix).
-#' It optionally estimates overdispersion based on the fitted model.
+#' `fit_devil` performs model fitting by estimating beta coefficients, dispersion parameters,
+#' and beta sigma. The function uses predictor variables provided in the `design_matrix` and a response
+#' variable provided in the `input_matrix`. Optional features include the estimation of overdispersion
+#' and the computation of size factors. The function supports parallel processing and allows customization
+#' of various parameters such as the number of iterations, convergence tolerance, and more.
 #'
-#' @param input_matrix Vector of response variable.
-#' @param design_matrix Matrix of predictor variables.
-#' @param overdispersion Logical indicating whether to estimate overdispersion.
-#' @param offset Offset vector to be included in the model. Defaults to 0.
-#' @param size_factors Logical indicating whether to compute size factors. Defaults to TRUE.
-#' @param avg_counts Minimum value of counts per cell required to fit a gene. Default is 0.001.
-#' @param min_cells Minimum value of cells with non-zero counts required to fit a gene. Default is 5.
-#' @param verbose Logical indicating whether to display progress messages. Defaults to FALSE.
-#' @param max_iter Maximum number of iterations for optimization. Defaults to 500.
-#' @param eps Tolerance level for convergence criterion. Defaults to 1e-4.
-#' @param parallel.cores Number of cores required for parallelization. Default is NULL, which use the maximum cores available.
-#' @return List containing fitted model parameters including beta coefficients and overdispersion parameter (if estimated).
+#' @param input_matrix A numeric matrix representing the response variable, with rows corresponding to genes and columns to samples.
+#' @param design_matrix A numeric matrix representing the predictor variables, with rows corresponding to samples and columns to predictors.
+#' @param overdispersion Logical value indicating whether to estimate the overdispersion parameter. (default is `TRUE`)
+#' @param offset A numeric vector to be included as an offset in the model. (default is `0`)
+#' @param size_factors Logical value indicating whether to compute size factors for normalization. (default is `TRUE`)
+#' @param verbose Logical value indicating whether to display progress messages during execution. (default is `FALSE`)
+#' @param max_iter Integer specifying the maximum number of iterations allowed for the optimization process. (default is `500`)
+#' @param tolerance Numeric value indicating the tolerance level for the convergence criterion. (default is `1e-3`)
+#' @param eps A small numeric value added to `input_matrix` to avoid issues with non-invertible matrices. (default is `1e-6`)
+#' @param parallel.cores Integer specifying the number of CPU cores to use for parallelization. If `NULL`, the maximum number of available cores are used. (defaults is `NULL`)
+#'
+#' @return A list containing the following elements:
+#' \item{beta}{A matrix of fitted beta coefficients for each gene.}
+#' \item{overdispersion}{A numeric vector of overdispersion parameters for each gene (if estimated).}
+#' \item{iterations}{A numeric vector indicating the number of iterations taken for each gene.}
+#' \item{size_factors}{A numeric vector of size factors used for normalization.}
+#' \item{offset_matrix}{A numeric matrix of offset values used in the model.}
+#' \item{design_matrix}{The design matrix provided as input.}
+#' \item{input_matrix}{The input matrix used after processing.}
+#' \item{input_parameters}{A list of input parameters used in the function, including `max_iter`, `tolerance`, and `parallel.cores`.}
+#'
 #' @export
 #' @rawNamespace useDynLib(devil);
-fit_devil <- function(input_matrix, design_matrix, overdispersion = TRUE, offset=0, size_factors=TRUE, avg_counts=.001, min_cells=5, verbose=FALSE, max_iter=500, eps=1e-4, parallel.cores=NULL) {
+fit_devil <- function(
+    input_matrix,
+    design_matrix,
+    overdispersion = TRUE,
+    offset=0,
+    size_factors=TRUE,
+    #avg_counts=.001,
+    #min_cells=5,
+    verbose=FALSE,
+    max_iter=500,
+    tolerance=1e-3,
+    eps=1e-6,
+    parallel.cores=NULL) {
 
   max.cores <- parallel::detectCores()
   if (is.null(parallel.cores)) {
@@ -36,23 +58,26 @@ fit_devil <- function(input_matrix, design_matrix, overdispersion = TRUE, offset
     n.cores = min(max.cores, parallel.cores)
   }
 
-  input_mat <- devil:::handle_input_matrix(input_matrix, verbose=verbose)
+  # Add epsilon to input_matrix to avoid non invertible matrices
+  input_matrix <- input_matrix + eps
+
+  input_mat <- handle_input_matrix(input_matrix, verbose=verbose)
 
   gene_names <- rownames(input_matrix)
-  counts_per_cell <- rowMeans(input_mat)
-  cell_per_genes <- rowSums(input_mat > 0)
-  filter_genes <- (counts_per_cell <= avg_counts) | (cell_per_genes <= min_cells)
-  n_low_genes <- sum(filter_genes)
-  if (n_low_genes > 0) {
-    message(paste0("Removing ", n_low_genes, " lowly expressed genes."))
-    input_mat <- matrix(input_mat[!filter_genes, ], ncol = nrow(design_matrix), nrow = sum(!filter_genes))
-    input_matrix <- input_matrix[!filter_genes, ]
-    gene_names <- gene_names[!filter_genes]
-  }
+  # counts_per_cell <- rowMeans(input_mat)
+  # cell_per_genes <- rowSums(input_mat > 0)
+  # filter_genes <- (counts_per_cell <= avg_counts) | (cell_per_genes <= min_cells)
+  # n_low_genes <- sum(filter_genes)
+  # if (n_low_genes > 0) {
+  #   message(paste0("Removing ", n_low_genes, " lowly expressed genes."))
+  #   input_mat <- matrix(input_mat[!filter_genes, ], ncol = nrow(design_matrix), nrow = sum(!filter_genes))
+  #   input_matrix <- input_matrix[!filter_genes, ]
+  #   gene_names <- gene_names[!filter_genes]
+  # }
 
   if (size_factors) {
     if (verbose) { message("Compute size factors") }
-    sf <- devil:::calculate_sf(input_matrix, verbose = verbose)
+    sf <- calculate_sf(input_matrix, verbose = verbose)
   } else {
     sf <- rep(1, nrow(design_matrix))
   }
@@ -64,50 +89,63 @@ fit_devil <- function(input_matrix, design_matrix, overdispersion = TRUE, offset
   nfeatures <- ncol(design_matrix)
 
   if (verbose) { message("Initialize beta estimate") }
-  groups <- devil:::get_groups_for_model_matrix(design_matrix)
+  # groups <- devil:::get_groups_for_model_matrix(design_matrix)
+  #
+  # if (!is.null(groups)) {
+  #   beta_0 <- devil:::init_beta_groups(input_mat, groups, offset_matrix)
+  # } else {
+  #   beta_0 <- devil:::init_beta(input_mat, design_matrix, offset_matrix)
+  # }
 
-  if (!is.null(groups)) {
-    beta_0 <- devil:::init_beta_groups(input_mat, groups, offset_matrix)
+  beta_0 <- devil:::init_beta(input_mat, design_matrix, offset_matrix)
 
-    if (verbose) { message("Fit beta coefficients") }
-    tmp <- parallel::mclapply(1:ngenes, function(i) {
-      r_groups <- lapply(unique(groups), function(g) {
-        group_filter <- groups == g
-        y <- input_mat[i,group_filter,drop=FALSE]
-        if (sum(y) != 0) {
-          devil:::beta_fit_group(y,
-                                 beta_0[i,g==unique(groups),drop=T],
-                                 offset_matrix[i,group_filter, drop=F],
-                                 dispersion_init[i],
-                                 max_iter = max_iter,
-                                 eps = eps)
-        } else {
-          list(mu_beta = -1e08, iter = 1)
-        }
+  if (verbose) { message("Fit beta coefficients") }
+  tmp <- parallel::mclapply(1:ngenes, function(i) {
+    devil:::beta_fit(input_mat[i,], design_matrix, beta_0[i,], offset_matrix[i,], dispersion_init[i], max_iter = max_iter, eps = tolerance)
+    #devil:::beta_fit(input_mat[i,], design_matrix, beta_0[i,], offset_matrix[i,], 1, max_iter = max_iter, eps = tolerance)
+  }, mc.cores = n.cores)
 
-      })
-      r <- list(
-        mu_beta = do.call(cbind, lapply(r_groups, function(x) {x$mu_beta})),
-        iter = mean(unlist(lapply(r_groups, function(x) {x$iter})))
-      )
-    }, mc.cores = n.cores)
-
-  } else {
-    beta_0 <- devil:::init_beta(input_mat, design_matrix, offset_matrix)
-
-    if (verbose) { message("Fit beta coefficients") }
-    tmp <- parallel::mclapply(1:ngenes, function(i) {
-      devil:::beta_fit(input_mat[i,], design_matrix, beta_0[i,], offset_matrix[i,], dispersion_init[i], max_iter = max_iter, eps = eps)
-    }, mc.cores = n.cores)
-  }
+  # if (!is.null(groups)) {
+  #   beta_0 <- init_beta_groups(input_mat, groups, offset_matrix)
+  #
+  #   if (verbose) { message("Fit beta coefficients") }
+  #   tmp <- parallel::mclapply(1:ngenes, function(i) {
+  #     r_groups <- lapply(unique(groups), function(g) {
+  #       group_filter <- groups == g
+  #       y <- input_mat[i,group_filter,drop=FALSE]
+  #       if (sum(y) != 0) {
+  #         beta_fit_group(y,
+  #                                beta_0[i,g==unique(groups),drop=T],
+  #                                offset_matrix[i,group_filter, drop=F],
+  #                                dispersion_init[i],
+  #                                max_iter = max_iter,
+  #                                eps = eps)
+  #       } else {
+  #         list(mu_beta = -1e08, iter = 1)
+  #       }
+  #
+  #     })
+  #     r <- list(
+  #       mu_beta = do.call(cbind, lapply(r_groups, function(x) {x$mu_beta})),
+  #       iter = mean(unlist(lapply(r_groups, function(x) {x$iter})))
+  #     )
+  #   }, mc.cores = n.cores)
+  #
+  # } else {
+  #   beta_0 <- init_beta(input_mat, design_matrix, offset_matrix)
+  #
+  #   if (verbose) { message("Fit beta coefficients") }
+  #   tmp <- parallel::mclapply(1:ngenes, function(i) {
+  #     beta_fit(input_mat[i,], design_matrix, beta_0[i,], offset_matrix[i,], dispersion_init[i], max_iter = max_iter, eps = tolerance)
+  #   }, mc.cores = n.cores)
+  # }
 
   beta <- lapply(1:ngenes, function(i) { tmp[[i]]$mu_beta }) %>% do.call("rbind", .)
   rownames(beta) <- gene_names
-
-  if (!(is.null(groups))) {
-    first_occurence_in_groups <- match(unique(groups), groups)
-    beta <- t(solve(design_matrix[first_occurence_in_groups, ,drop=FALSE], t(beta)))
-  }
+  # if (!(is.null(groups))) {
+  #   first_occurence_in_groups <- match(unique(groups), groups)
+  #   beta <- t(solve(design_matrix[first_occurence_in_groups, ,drop=FALSE], t(beta)))
+  # }
 
   iterations <- lapply(1:ngenes, function(i) { tmp[[i]]$iter }) %>% unlist()
 
@@ -130,7 +168,7 @@ fit_devil <- function(input_matrix, design_matrix, overdispersion = TRUE, offset
     offset_matrix=offset_matrix,
     design_matrix=design_matrix,
     input_matrix=input_mat,
-    input_parameters=list(max_iter=max_iter, eps=eps, parallel.cores=n.cores)
+    input_parameters=list(max_iter=max_iter, tolerance=tolerance, parallel.cores=n.cores)
     )
   )
 }
