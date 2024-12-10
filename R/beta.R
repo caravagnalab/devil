27570a3bfc21720a46cf4c6d246ee0d14588f8d0
Giovanni Santacatterina <65920542+jovoni@users.noreply.github.com>
2024-12-10 10:22:09 +0100

    Minor changes

--- R/beta.R
+++ R/beta.R
@@ -4,7 +4,8 @@ init_beta <- function(y, design_matrix, offset_matrix) {
   Q <- qr.Q(qrx)[seq_len(nrow(design_matrix)),,drop=FALSE]
   R <- qr.R(qrx)
 
-  norm_log_count_mat <- t(log1p((y / exp(offset_matrix))))
+  #norm_log_count_mat <- t(log1p((y / exp(offset_matrix))))
+  norm_log_count_mat <- log1p((t(y) / exp(offset_matrix)))
   #norm_log_count_mat <- t(log1p((y / exp(offset_matrix))))
   t(solve(R, as.matrix(t(Q) %*% norm_log_count_mat)))
 }
